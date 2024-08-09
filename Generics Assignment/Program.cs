2b7aa0c9942f819766330ba275f0a7966df1871d
AliElshenwy <elshenwy.01090555@gmail.com>
2024-08-09 17:55:00 +0300

    Example Out Put

--- Generics Assignment/Program.cs
+++ Generics Assignment/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             var intRange = new Range<int>(6, 10);
-            Console.WriteLine(intRange.IsInRange(11));   
-            Console.WriteLine(intRange.Length());       
+            Console.WriteLine(intRange.IsInRange(11));
+            Console.WriteLine(intRange.Length());
 
             var doubleRange = new Range<double>(3.5, 7.2);
-            Console.WriteLine(doubleRange.IsInRange(4.1)); 
-            Console.WriteLine(doubleRange.Length());       
+            Console.WriteLine(doubleRange.IsInRange(4.1));
+            Console.WriteLine(doubleRange.Length());
         }
     }
 }
