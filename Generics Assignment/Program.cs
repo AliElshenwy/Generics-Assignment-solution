f058719ce0fb5340a07efc76a3eb5a7f9e9008e5
AliElshenwy <elshenwy.01090555@gmail.com>
2024-08-09 17:41:34 +0300

    Add project files.

---
+++ Generics Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Generics_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intRange = new Range<int>(6, 10);
+            Console.WriteLine(intRange.IsInRange(11));   
+            Console.WriteLine(intRange.Length());       
+
+            var doubleRange = new Range<double>(3.5, 7.2);
+            Console.WriteLine(doubleRange.IsInRange(4.1)); 
+            Console.WriteLine(doubleRange.Length());       
+        }
+    }
+}
---
+++ Generics Assignment/Range.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+
+        public T MiniMumValue { get; set; }
+        public T MaxiMumValue { get; set; }
+
+
+        public Range(T minValue, T maxValue)
+        {
+            MiniMumValue = minValue;
+            MaxiMumValue = maxValue;
+        }
+
+        public bool IsInRange(T Value)
+        {
+            return Value.CompareTo(MiniMumValue) > 0 && Value.CompareTo(MaxiMumValue) < 0;
+
+        }
+        public T Length()
+        {
+
+            dynamic min = MiniMumValue;
+            dynamic max = MaxiMumValue;
+            return max - min;
+        }
+
+        
+
+
+
+    }
+
+    
+}
